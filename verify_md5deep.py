b3cd3a8fae2f2da4d67dfa3dbeab38064b186abe
George Dubinin <gtdubinin@ucsd.edu>
2024-08-15 20:30:52 -0400

    Modified script to support OpenRVDAS/OpenVDM style md5deep files (i.e. md5_summary.txt where there's only two columns instead of 4).

--- verify_md5deep.py
+++ verify_md5deep.py
@@ -14,18 +14,33 @@ def fix_second(path):
     return '/'.join(files)[1:]
 
 
-def process_file(file_name):
+def process_file(file_name, hash_only, path_only):
     file_set = set()
     with open(file_name) as f:
         for line in f:
-            split_1 = line.split(',')
-            split_2 = line.split('  .')
-            if len(split_2) == 2:
+            split_1 = line.split(',') # This variable be a list of substrings of the line separated by commas (not including the commas).
+            split_2 = line.split(' ') # This variable is useful for manifest files that contain only two columns (hash, filepath) separated by a space.
+
+            # Determine which format of md5deep file we are dealing with and construct (hash, filepath) pairs for each line accordingly.
+            # This is the regular md5deep format that you get by running the md5deep command. 
+            if len(split_1) == 4:
+                pair = (split_1[1].strip(), split_1[3].strip())
+            # Distros in the OpenVDM/OpenRVDAS format will have an md5deep file called "md5_summary.txt"
+            # and these are different because they don't only have 2 columns.
+            elif len(split_2) == 2:
                 pair = (split_2[0].strip(), split_2[1].strip())
-                file_set.add(pair)
             else:
-                pair = (split_1[1].strip(), fix_second(split_1[3]).strip())
-                file_set.add(pair)
+                # If we've not found either 2 or 4 commas then I don't recognize this manifest file so just skip this line.
+                continue
+
+            # If the user has elected to not check paths then set the path part of the pair to the same thing for all pairs.
+            if hash_only:
+                pair = (pair[0], "ignore me")
+
+            # In a similar fashion if the user has specified to not check hashes then replace all hashes with a constant so that they'll all match.
+            if path_only:
+                pair = ("ignore me", pair[1])
+            file_set.add(pair)
 
     return file_set
 
@@ -55,6 +70,10 @@ def main():
                         help='show all files')
     parser.add_argument('-c',nargs=1,metavar='num',
                         help='copy diff of files in a given direction')
+    parser.add_argument('-f',action='store_true',
+                        help='only compare hashes')
+    parser.add_argument('-q',action='store_true',
+                        help='only compare filepaths')
     args = parser.parse_args()
 
     date = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
@@ -62,18 +81,18 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(levelname)-8s - %(message)s',
                         level=logging.DEBUG,
                         handlers=[console_handler])
-
-    set1 = process_file(args.file1)
-    set2 = process_file(args.file2)
+    
+    set1 = process_file(args.file1, args.f, args.q)
+    set2 = process_file(args.file2, args.f, args.q)
 
     if len(set1) == len(set2) and set1 == set2:
         logging.info('File 1 and file 2 are the same')
     else:
         if args.a:
             for item in set1 - set2:
-                logging.info(f'1 - 2: {item}')
+                logging.info(f'FILE THAT IS IN FILE 1 AND NOT IN FILE 2: {item}')
             for item in set2 - set1:
-                logging.info(f'2 - 1: {item}')
+                logging.info(f'FILE THAT IS IN FILE 2 AND NOT IN FILE 1: {item}')
         logging.info(f'File 1 contains {len(set1)} files and has {len(set1 - set2)} files that file 2 does not (1 - 2)')
         logging.info(f'File 2 contains {len(set2)} files and has {len(set2 - set1)} files that file 1 does not (2 - 1)')
     
